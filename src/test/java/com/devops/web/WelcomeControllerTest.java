1012fa14ac2e03ee3613f4d6aa15e5c18d5fcaed
tejasvihegde <tejasvi.qa@gmail.com>
2020-11-01 13:24:39 -0500

    WebPage & controller with test

---
+++ src/test/java/com/devops/web/WelcomeControllerTest.java
@@ -0,0 +1,43 @@
+package com.devops.web;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+class WelcomeControllerTest {
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext context;
+	
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+
+	@Test
+	void test() throws Exception {
+		
+		mockMvc.perform(get("/")).andExpect(status().isOk())
+			.andExpect(model().attribute("course", containsString("DevOps")));
+	
+	}
+
+}
